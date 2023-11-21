c7a7936b6c0b7200a863e7367d8082b4785ae039
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-11-21 10:15:19 -0700

    adds search and pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -18,7 +18,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   };
  
   // ...
-}
+
     // if (pageNumber === 1) {
     //   params.delete('page');
     // } else {
@@ -27,13 +27,13 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
     // return `${pathname}?${params.toString()}`;
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -66,7 +66,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 
@@ -136,3 +136,5 @@ function PaginationArrow({
     </Link>
   );
 }
+}
+
