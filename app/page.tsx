e49740d155756f44b17916249d38de5847544506
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-12-16 08:05:04 -0700

    finishes with metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
 
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
